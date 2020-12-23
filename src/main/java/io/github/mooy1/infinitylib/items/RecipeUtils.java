49d92e177d74e2d09f537a44e05ab7d0ecb0d9b5
Mooy1 <69326411+Mooy1@users.noreply.github.com>
2020-12-22 23:20:11 -0600

    e

--- src/main/java/io/github/mooy1/infinitylib/items/RecipeUtils.java
+++ src/main/java/io/github/mooy1/infinitylib/items/RecipeUtils.java
@@ -2,6 +2,7 @@ package io.github.mooy1.infinitylib.items;
 
 import io.github.mooy1.infinitylib.filter.ItemFilter;
 import io.github.mooy1.infinitylib.filter.MultiFilter;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 
@@ -11,7 +12,8 @@ public final class RecipeUtils {
         ItemFilter[] array = new ItemFilter[9];
         for (int i = 0 ; i < recipe.getShape().length ; i++) {
             for (int j = 0 ; j < recipe.getShape()[i].length() ; j++) {
-                array[(i + 1) * (j + 1) - 1] = new ItemFilter(recipe.getIngredientMap().get(recipe.getShape()[i].charAt(j))); 
+                ItemStack item = recipe.getIngredientMap().get(recipe.getShape()[i].charAt(j));
+                array[(i + 1) * (j + 1) - 1] = item != null ? new ItemFilter(item) : null; 
             }
         }
         return new MultiFilter(array);
@@ -20,7 +22,8 @@ public final class RecipeUtils {
     public static MultiFilter getRecipeFilter(ShapelessRecipe recipe) {
         ItemFilter[] array = new ItemFilter[9];
         for (int i = 0 ; i < recipe.getIngredientList().size() ; i++) {
-            array[i] = new ItemFilter(recipe.getIngredientList().get(i));
+            ItemStack item = recipe.getIngredientList().get(i);
+            array[i] = item != null ? new ItemFilter(item) : null;
         }
         return new MultiFilter(array);
     }
