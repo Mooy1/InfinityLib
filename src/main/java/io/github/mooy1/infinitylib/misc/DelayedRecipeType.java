2dd41ae806dfb2256d9b841c43bde49c30fb9ec0
Mooy1 <69326411+Mooy1@users.noreply.github.com>
2021-01-15 12:00:30 -0600

    e

---
+++ src/main/java/io/github/mooy1/infinitylib/misc/DelayedRecipeType.java
@@ -0,0 +1,28 @@
+package io.github.mooy1.infinitylib.misc;
+
+import io.github.mooy1.infinitylib.PluginUtils;
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Locale;
+import java.util.function.BiConsumer;
+
+public final class DelayedRecipeType extends RecipeType {
+
+    private final DelayedConsumer<ItemStack[], ItemStack> delayed;
+    
+    public DelayedRecipeType(SlimefunItemStack item) {
+        this(item, new DelayedConsumer<>());
+    }
+
+    private DelayedRecipeType(SlimefunItemStack item, DelayedConsumer<ItemStack[], ItemStack> consumer) {
+        super(PluginUtils.getKey(item.getItemId().toLowerCase(Locale.ROOT)), item, consumer);
+        this.delayed = consumer;
+    }
+    
+    public void acceptEach(BiConsumer<ItemStack[], ItemStack> consumer) {
+        this.delayed.acceptEach(consumer);
+    }
+    
+}
