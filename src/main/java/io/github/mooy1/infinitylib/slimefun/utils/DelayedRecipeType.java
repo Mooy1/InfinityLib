b61bfc7f876b463162cf989d5f470d9d8423caa9
Mooy1 <bobbypantseats@gmail.com>
2021-03-22 12:43:12 -0500

    Update DelayedRecipeType.java

--- src/main/java/io/github/mooy1/infinitylib/slimefun/utils/DelayedRecipeType.java
+++ src/main/java/io/github/mooy1/infinitylib/slimefun/utils/DelayedRecipeType.java
@@ -17,8 +17,8 @@ import java.util.function.BiConsumer;
  * @author Mooy1
  */
 public final class DelayedRecipeType extends RecipeType {
-    
-    private DelayedConsumer consumer;
+
+    private final DelayedConsumer consumer;
     
     public DelayedRecipeType(SlimefunItemStack item) {
         this(item, new DelayedConsumer());
@@ -30,28 +30,28 @@ public final class DelayedRecipeType extends RecipeType {
     }
     
     public void accept(BiConsumer<ItemStack[], ItemStack> consumer) {
-        if (this.consumer == null) {
-            throw new IllegalStateException("The recipes of this RecipeType have already been accepted!");
+        // add to acceptors
+        this.consumer.acceptors.add(consumer);
+        
+        // accept all current
+        for (Pair<ItemStack[], ItemStack> pair : this.consumer.toBeAccepted) {
+            consumer.accept(pair.getFirstValue(), pair.getSecondValue());
         }
-        for (Pair<ItemStack[], ItemStack> stacks : this.consumer.toBeAccepted) {
-            consumer.accept(stacks.getFirstValue(), stacks.getSecondValue());
-        }
-        this.consumer.consumer = consumer;
-        this.consumer.toBeAccepted = null;
-        this.consumer = null;
     }
     
     private static final class DelayedConsumer implements BiConsumer<ItemStack[], ItemStack> {
 
-        private List<Pair<ItemStack[], ItemStack>> toBeAccepted = new ArrayList<>();
-        private BiConsumer<ItemStack[], ItemStack> consumer = null;
+        private final List<BiConsumer<ItemStack[], ItemStack>> acceptors = new ArrayList<>();
+        private final List<Pair<ItemStack[], ItemStack>> toBeAccepted = new ArrayList<>();
 
         @Override
         public void accept(ItemStack[] stacks, ItemStack itemStack) {
-            if (this.consumer == null) {
-                this.toBeAccepted.add(new Pair<>(stacks, itemStack));
-            } else {
-                this.consumer.accept(stacks, itemStack);
+            // add to all accepted
+            this.toBeAccepted.add(new Pair<>(stacks, itemStack));
+            
+            // accept to all acceptors
+            for (BiConsumer<ItemStack[], ItemStack> acceptor : this.acceptors) {
+                acceptor.accept(stacks, itemStack);
             }
         }
 
