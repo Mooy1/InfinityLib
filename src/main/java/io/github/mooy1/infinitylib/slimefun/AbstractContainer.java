eda888cbd48f989f68a87931bc4b9c98a153a177
Riley <69326411+Mooy1@users.noreply.github.com>
2021-05-20 18:15:56 -0500

    a

--- src/main/java/io/github/mooy1/infinitylib/slimefun/AbstractContainer.java
+++ src/main/java/io/github/mooy1/infinitylib/slimefun/AbstractContainer.java
@@ -1,26 +1,27 @@
 package io.github.mooy1.infinitylib.slimefun;
 
-import java.util.List;
-import javax.annotation.Nonnull;
-
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
-
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
 import me.mrCookieSlime.Slimefun.api.inventory.DirtyChestMenu;
 import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
 import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectableAction;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * A slimefun item with a menu
@@ -63,22 +64,43 @@ public abstract class AbstractContainer extends SlimefunItem {
         };
         
         addItemHandler(new BlockBreakHandler(false, false) {
+
             @Override
             public void onPlayerBreak(@Nonnull BlockBreakEvent e, @Nonnull ItemStack itemStack, @Nonnull List<ItemStack> list) {
                 onBreak(e);
             }
+
         });
         
         addItemHandler(new BlockPlaceHandler(false) {
+
             @Override
             public void onPlayerPlace(@Nonnull BlockPlaceEvent e) {
                 onPlace(e);
             }
+
+        });
+
+        addItemHandler(new BlockTicker() {
+
+            @Override
+            public boolean isSynchronized() {
+                return AbstractContainer.this.isSynchronized();
+            }
+
+            @Override
+            public void tick(Block b, SlimefunItem item, Config data) {
+                AbstractContainer.this.tick(b);
+            }
+
         });
     }
 
+
     @Nonnull
     protected abstract int[] getTransportSlots(@Nonnull DirtyChestMenu menu, @Nonnull  ItemTransportFlow flow, ItemStack item);
+
+    protected abstract void tick(@Nonnull Block b);
 
     protected abstract void setupMenu(@Nonnull BlockMenuPreset preset);
     
@@ -92,6 +114,10 @@ public abstract class AbstractContainer extends SlimefunItem {
 
     protected void onPlace(@Nonnull BlockPlaceEvent e) {
 
+    }
+
+    protected boolean isSynchronized() {
+        return false;
     }
     
 }
