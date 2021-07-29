b016c9528802d276b45507f8315be5f991d95331
Riley <69326411+Mooy1@users.noreply.github.com>
2021-07-28 23:23:57 -0500

    Update StackUtils.java

--- src/main/java/io/github/mooy1/infinitylib/items/StackUtils.java
+++ src/main/java/io/github/mooy1/infinitylib/items/StackUtils.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Function;
 
 import javax.annotation.Nonnull;
