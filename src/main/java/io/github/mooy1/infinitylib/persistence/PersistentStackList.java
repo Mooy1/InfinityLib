90f875af42ad6b3ab74a5ae136b3ba7850b4d9c2
Riley <69326411+Mooy1@users.noreply.github.com>
2021-08-05 12:17:30 -0500

    changed persistence stuff to use byte[] as primitive and lists

--- src/main/java/io/github/mooy1/infinitylib/persistence/PersistentStackList.java
+++ src/main/java/io/github/mooy1/infinitylib/persistence/PersistentStackList.java
@@ -2,16 +2,23 @@ package io.github.mooy1.infinitylib.persistence;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
-import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
-final class PersistentLocation implements PersistentDataType<byte[], Location> {
+/**
+ * Persistent data type for ItemStack arrays
+ *
+ * @author Mooy1
+ */
+final class PersistentStackList implements PersistentDataType<byte[], List<ItemStack>> {
 
     @Nonnull
     @Override
@@ -21,17 +28,20 @@ final class PersistentLocation implements PersistentDataType<byte[], Location> {
 
     @Nonnull
     @Override
-    public Class<Location> getComplexType() {
-        return Location.class;
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public Class<List<ItemStack>> getComplexType() {
+        return (Class) List.class;
     }
 
     @Nonnull
     @Override
-    public byte[] toPrimitive(@Nonnull Location complex, @Nonnull PersistentDataAdapterContext context) {
+    public byte[] toPrimitive(@Nonnull List<ItemStack> complex, @Nonnull PersistentDataAdapterContext context) {
         try {
             ByteArrayOutputStream bytes = new ByteArrayOutputStream();
             BukkitObjectOutputStream output = new BukkitObjectOutputStream(bytes);
-            output.writeObject(complex);
+            for (ItemStack item : complex) {
+                output.writeObject(item);
+            }
             return bytes.toByteArray();
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,14 +51,18 @@ final class PersistentLocation implements PersistentDataType<byte[], Location> {
 
     @Nonnull
     @Override
-    public Location fromPrimitive(@Nonnull byte[] primitive, @Nonnull PersistentDataAdapterContext context) {
+    public List<ItemStack> fromPrimitive(@Nonnull byte[] primitive, @Nonnull PersistentDataAdapterContext context) {
         try {
             ByteArrayInputStream bytes = new ByteArrayInputStream(primitive);
             BukkitObjectInputStream input = new BukkitObjectInputStream(bytes);
-            return (Location) input.readObject();
+            List<ItemStack> list = new ArrayList<>();
+            while (bytes.available() > 0) {
+                list.add((ItemStack) input.readObject());
+            }
+            return list;
         } catch (Exception e) {
             e.printStackTrace();
-            return new Location(null, 0, 0, 0);
+            return new ArrayList<>();
         }
     }
 
---
+++ src/main/java/io/github/mooy1/infinitylib/persistence/PersistentStringList.java
@@ -0,0 +1,63 @@
+package io.github.mooy1.infinitylib.persistence;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+
+final class PersistentStringList implements PersistentDataType<byte[], List<String>> {
+
+    @Nonnull
+    @Override
+    public Class<byte[]> getPrimitiveType() {
+        return byte[].class;
+    }
+
+    @Nonnull
+    @Override
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public Class<List<String>> getComplexType() {
+        return (Class) List.class;
+    }
+
+    @Nonnull
+    @Override
+    public byte[] toPrimitive(@Nonnull List<String> complex, @Nonnull PersistentDataAdapterContext context) {
+        try {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream(complex.size() * 20);
+            for (String string : complex) {
+                byte[] arr = string.getBytes();
+                bytes.write(arr.length);
+                bytes.write(arr, 0, arr.length);
+            }
+            return bytes.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+
+    @Nonnull
+    @Override
+    public List<String> fromPrimitive(@Nonnull byte[] primitive, @Nonnull PersistentDataAdapterContext context) {
+        try {
+            ByteArrayInputStream bytes = new ByteArrayInputStream(primitive);
+            List<String> list = new ArrayList<>(primitive.length / 20);
+            while (bytes.available() > 0) {
+                byte[] arr = new byte[bytes.read()];
+                bytes.readNBytes(arr, 0, arr.length);
+                list.add(new String(arr));
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+}
