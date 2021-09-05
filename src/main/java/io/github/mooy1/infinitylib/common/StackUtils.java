0cb1c62b595b6dd9af41c4a5b546a084d9ba7acc
Riley <69326411+Mooy1@users.noreply.github.com>
2021-09-05 12:30:20 -0500

    added getIdOrType, itemById, itemByIdOrType

--- src/main/java/io/github/mooy1/infinitylib/common/StackUtils.java
+++ src/main/java/io/github/mooy1/infinitylib/common/StackUtils.java
@@ -1,15 +1,18 @@
 package io.github.mooy1.infinitylib.common;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import lombok.experimental.UtilityClass;
 
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 
@@ -33,6 +36,30 @@ public final class StackUtils {
     @Nullable
     public static String getId(ItemMeta meta) {
         return meta.getPersistentDataContainer().get(ID_KEY, PersistentDataType.STRING);
+    }
+
+    @Nonnull
+    public static String getIdOrType(ItemStack item) {
+        if (item instanceof SlimefunItemStack) {
+            return ((SlimefunItemStack) item).getItemId();
+        } else if (item.hasItemMeta()) {
+            String id = getId(item.getItemMeta());
+            return id == null ? item.getType().name() : id;
+        } else {
+            return item.getType().name();
+        }
+    }
+
+    @Nullable
+    public static ItemStack itemById(String id) {
+        SlimefunItem item = SlimefunItem.getById(id);
+        return item == null ? null : item.getItem().clone();
+    }
+
+    @Nonnull
+    public static ItemStack itemByIdOrType(String idOrType) {
+        SlimefunItem item = SlimefunItem.getById(idOrType);
+        return item == null ? new ItemStack(Material.valueOf(idOrType)) : item.getItem().clone();
     }
 
 }
--- src/test/java/io/github/mooy1/infinitylib/common/TestStackUtils.java
+++ src/test/java/io/github/mooy1/infinitylib/common/TestStackUtils.java
@@ -11,13 +11,14 @@ import be.seeseemelk.mockbukkit.MockBukkit;
 import io.github.thebusybiscuit.slimefun4.core.services.CustomItemDataService;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.implementation.setup.SlimefunItemSetup;
 
 public final class TestStackUtils {
 
     @BeforeAll
     public static void load() {
         MockBukkit.mock();
-        MockBukkit.load(Slimefun.class);
+        SlimefunItemSetup.setup(MockBukkit.load(Slimefun.class));
     }
 
     @AfterAll
@@ -35,6 +36,37 @@ public final class TestStackUtils {
         Assertions.assertEquals(StackUtils.getId(item2), dataService.getItemData(item2).orElse(null));
         Assertions.assertEquals(StackUtils.getId(item1.getItemMeta()), dataService.getItemData(item1.getItemMeta()).orElse(null));
         Assertions.assertEquals(StackUtils.getId(item2.getItemMeta()), dataService.getItemData(item2.getItemMeta()).orElse(null));
+    }
+
+    @Test
+    void testGetIdOrType() {
+        CustomItemDataService dataService = Slimefun.getItemDataService();
+        ItemStack item1 = SlimefunItems.ADVANCED_CIRCUIT_BOARD;
+        ItemStack item2 = new ItemStack(Material.IRON_BLOCK);
+
+        Assertions.assertEquals(StackUtils.getIdOrType(item1), dataService.getItemData(item1).orElse(item1.getType().name()));
+        Assertions.assertEquals(StackUtils.getIdOrType(item2), dataService.getItemData(item2).orElse(item2.getType().name()));
+    }
+
+    @Test
+    void testItemById() {
+        ItemStack item1 = SlimefunItems.ADVANCED_CIRCUIT_BOARD;
+        String id1 = SlimefunItems.ADVANCED_CIRCUIT_BOARD.getItemId();
+        String id2 = Material.IRON_BLOCK.name();
+
+        Assertions.assertEquals(StackUtils.itemById(id1), item1);
+        Assertions.assertNull(StackUtils.itemById(id2));
+    }
+
+    @Test
+    void testItemByIdOrType() {
+        ItemStack item1 = SlimefunItems.ADVANCED_CIRCUIT_BOARD;
+        ItemStack item2 = new ItemStack(Material.IRON_BLOCK);
+        String id1 = SlimefunItems.ADVANCED_CIRCUIT_BOARD.getItemId();
+        String id2 = item2.getType().name();
+
+        Assertions.assertEquals(StackUtils.itemByIdOrType(id1), item1);
+        Assertions.assertEquals(StackUtils.itemByIdOrType(id2), item2);
     }
 
 }
